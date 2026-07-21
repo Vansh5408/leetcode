ab128cbb54fd820025feafc7cb2fd075f05480e8
Vansh jindal <vanshjindal1450@gmail.com>
2026-07-21 11:09:38 +0530

    Time: 8 ms (92.10%) | Memory: 49.3 MB (27.40%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals,(a,b)->{
+            if(a[0]==b[0]) return b[0]-a[0];
+            else return a[0]-b[0];
+        });
+        ArrayList<ArrayList<Integer>>ans = new ArrayList<>();
+        int first=intervals[0][0];
+        int last = intervals[0][1];
+        int a=0;
+        for(int i=1;i<intervals.length;i++){
+            if(intervals[i][0]>=first && intervals[i][0]<=last){
+                if(last<=intervals[i][1]){
+                    last=intervals[i][1];
+                }
+                else{
+                    continue;
+                }
+            }
+            else{
+                ans.add(new ArrayList<>());
+                ans.get(a).add(first);
+                ans.get(a).add(last);
+                a++;
+                first=intervals[i][0];
+                last=intervals[i][1];
+            }
+        }
+        ans.add(new ArrayList<>());
+        ans.get(a).add(first);
+        ans.get(a).add(last);
+        int [][]result = new int [ans.size()][2];
+        for(int i=0;i<result.length;i++){
+            result[i][0]=ans.get(i).get(0);
+            result[i][1]=ans.get(i).get(1);
+        }
+        return result;
+    }
+}
