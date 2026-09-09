79054a428b4435105d9231aa0e43f33b61aa281e
Vansh jindal <vanshjindal1450@gmail.com>
2026-09-09 12:00:07 +0530

    Time: 12 ms (5.06%) | Memory: 49.2 MB (15.84%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.java
+++ 547-number-of-provinces/number-of-provinces.java
@@ -1,22 +1,21 @@
 class Solution {
-    public int findCircleNum(int[][] isConnected) {
-        HashMap<Integer, ArrayList<Integer>> map = new HashMap<>();
-        for (int i = 0; i < isConnected.length; i++) {
-            map.put(i + 1, new ArrayList<>());
-            for (int j = 0; j < isConnected[0].length; j++) {
-                if (isConnected[i][j] == 1) {
-                    map.get(i + 1).add(j + 1);
+    public int findCircleNum(int[][] arr) {
+        HashMap<Integer,ArrayList<Integer>>map = new HashMap<>();
+        for(int i=0;i<arr.length;i++){
+            map.put(i+1,new ArrayList<>());
+            for(int j=0;j<arr[0].length;j++){
+                if(arr[i][j]==1){
+                    map.get(i+1).add(j+1);
                 }
             }
         }
-        int ans = Count(map);
-        return ans;
-    }
+        return connect(map);
 
-    public int Count(HashMap<Integer, ArrayList<Integer>> map) {
+    }
+    public int connect(HashMap<Integer,ArrayList<Integer>> map){
+        HashSet<Integer> set = new HashSet<>();
+        Queue<Integer> q = new LinkedList<>();
         int ans =0;
-        HashSet<Integer>set = new HashSet<>();
-        Queue<Integer>q= new LinkedList<>();
         for(int i=1;i<=map.size();i++){
             if(set.contains(i)){
                 continue;
@@ -29,7 +28,7 @@ class Solution {
                 }
                 set.add(rv);
                 for(int j=0;j<map.get(rv).size();j++){
-                    int edge=map.get(rv).get(j);
+                    int edge = map.get(rv).get(j);
                     if(!set.contains(edge)){
                         q.add(edge);
                     }
