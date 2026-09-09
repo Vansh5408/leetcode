0b84e0ed6f9d46f20f2ad038bfd77702e605d6fd
Vansh jindal <vanshjindal1450@gmail.com>
2026-09-10 05:07:28 +0530

    Time: 679 ms (5.02%) | Memory: 43.2 MB (70.35%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,35 +1,12 @@
 class Solution {
-
     public int findTargetSumWays(int[] nums, int target) {
-
-        int sum = 0;
-        for (int x : nums)
-            sum += x;
-
-        if (Math.abs(target) > sum)
-            return 0;
-
-        if ((sum + target) % 2 != 0)
-            return 0;
-
-        int req = (sum + target) / 2;
-
-        return find(nums, req, 0);
+        return sum(nums,target,0,0);
     }
-
-    public int find(int[] arr, int sum, int i) {
-
-        if (i == arr.length) {
-            return sum == 0 ? 1 : 0;
-        }
-
-        int first = 0;
-
-        if (arr[i] <= sum)
-            first = find(arr, sum - arr[i], i + 1);
-
-        int second = find(arr, sum, i + 1);
-
-        return first + second;
+    public int sum(int []arr,int k,int i,int curr){
+        if(curr==k && i==arr.length) return 1;
+        if(i>=arr.length) return 0;
+        int first = sum(arr,k,i+1,curr+arr[i]);
+        int second =sum(arr,k,i+1,curr-arr[i]);
+        return first+second;
     }
 }
