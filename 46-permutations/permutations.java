c65fea591bda76b74194849d4badfcb22010ec02
Vansh jindal <vanshjindal1450@gmail.com>
2026-09-13 10:27:25 +0530

    Time: 1 ms (88.01%) | Memory: 45.4 MB (78.52%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,22 +1,25 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>>ans = new ArrayList<>();
-        boolean []arr = new boolean [nums.length];
-        swap(ans,nums,arr,new ArrayList());
-        return ans;
+        boolean []temp = new boolean [nums.length];
+        Arrays.fill(temp,false);
+        List<List<Integer>> ans= new ArrayList<>();
+        return permutation(ans,new ArrayList<>(),temp,nums);
     }
-    public void swap(List<List<Integer>>ans,int []nums,boolean []arr,List<Integer>l){
-        if(l.size()==nums.length){
-            ans.add(new ArrayList(l));
-            return;
+    public List<List<Integer>> permutation(List<List<Integer>>ans,List<Integer>l,boolean[]temp,int []arr){
+
+        if(l.size()==arr.length){
+            ans.add(new ArrayList<>(l));
+            return ans;
         }
-        for(int i=0;i<nums.length;i++){
-            if(arr[i]) continue;
-            arr[i]=true;
-            l.add(nums[i]);
-            swap(ans,nums,arr,l);
-            l.remove(l.size()-1);
-            arr[i]=false;
+        for(int i=0;i<arr.length;i++){
+            if(temp[i]!=true){
+                temp[i]=true;
+                l.add(arr[i]);
+                permutation(ans,l,temp,arr);
+                l.remove(l.size()-1);
+                temp[i]=false;
+            }
         }
+        return ans;
     }
 }
