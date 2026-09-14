ef6fd914a7a33fc645492029a92f48541038ad9a
Vansh jindal <vanshjindal1450@gmail.com>
2026-09-14 12:41:02 +0530

    Time: 420 ms (5.01%) | Memory: 46.8 MB (10.27%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,5 +1,6 @@
 class Solution { 
-    public boolean exist(char[][] board, String word) { 
+    public boolean exist(char[][] board, String word) {
+        if(board.length*board[0].length<word.length()) return false; 
         for(int i=0;i<board.length;i++){ 
             for(int j=0;j<board[0].length;j++){ 
                 if(board[i][j]==word.charAt(0)){ 
