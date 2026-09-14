2a94a3652220064de865b07f646d326cb2e2fc46
Vansh jindal <vanshjindal1450@gmail.com>
2026-09-14 12:39:30 +0530

    Time: 448 ms (5.01%) | Memory: 46.6 MB (13.55%) - LeetSync

---
+++ 79-word-search/word-search.java
@@ -0,0 +1,37 @@
+class Solution { 
+    public boolean exist(char[][] board, String word) { 
+        for(int i=0;i<board.length;i++){ 
+            for(int j=0;j<board[0].length;j++){ 
+                if(board[i][j]==word.charAt(0)){ 
+                    if(ans(board,i,j,word)) return true; 
+                } 
+            } 
+        } 
+        return false; 
+    } 
+
+    public boolean ans(char[][] board,int i,int j,String word){ 
+        
+        if(word.length()==0) return true;
+
+        if(i<0 || i>=board.length || j<0 || j>=board[0].length){
+            return false;
+        }
+
+        if(board[i][j]!=word.charAt(0)){
+            return false;
+        }
+
+        char temp = board[i][j];
+        board[i][j] = '#';
+
+        boolean first = ans(board,i+1,j,word.substring(1));
+        boolean second = ans(board,i-1,j,word.substring(1));
+        boolean third = ans(board,i,j+1,word.substring(1));
+        boolean forth = ans(board,i,j-1,word.substring(1));
+
+        board[i][j] = temp;
+
+        return first || second || third || forth;
+    }
+}
