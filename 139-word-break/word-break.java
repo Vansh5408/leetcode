4168e88aa9e0fe0b2263a37396e884cab633bb29
Vansh jindal <vanshjindal1450@gmail.com>
2026-09-18 07:26:15 +0530

    Time: 6 ms (66.16%) | Memory: 46.2 MB (34.45%) - LeetSync

--- 139-word-break/word-break.java
+++ 139-word-break/word-break.java
@@ -1,33 +1,22 @@
-class Solution { 
-    public boolean wordBreak(String s, List<String> arr) { 
-        Boolean[] dp = new Boolean[s.length() + 1]; 
-        return word(s, arr, 0, dp); 
-    } 
-
-    public boolean word(String s, List<String> arr, int start, Boolean[] dp) { 
-
-        if (start == s.length()) {
-            return true; 
+class Solution {
+    public boolean wordBreak(String s, List<String> arr) {
+        Boolean []dp = new Boolean[s.length()+1];
+        return word(s,arr,0,dp);
+    }
+    public boolean word(String s,List<String>arr, int i,Boolean []dp){
+        if(i==s.length()) return true;
+        if(dp[i]!=null){
+            return dp[i];
         }
-
-        if (dp[start] != null) { 
-            return dp[start]; 
-        }
-
-        for (int end = start + 1; end <= s.length(); end++) { 
-
-            String temp = s.substring(start, end); 
-
-            if (arr.contains(temp)) { 
-
-                boolean ans = word(s, arr, end, dp); 
-
-                if (ans) {
-                    return dp[start] = true;
+        for(int j=i;j<s.length();j++){
+            String temp=s.substring(i,j+1);
+            if(arr.contains(temp)){
+                boolean ans =word(s,arr,j+1,dp);
+                if(ans){
+                    return dp[i]= true;
                 }
-            } 
-        } 
-
-        return dp[start] = false; 
-    } 
+            }
+        }
+        return dp[i]=false;
+    }
 }
